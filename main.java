e9e4386f24e39e1edc99bb8c31ce65b1f00bd5a0
Bruno-Grangeiro <109104329+Bruno-Grangeiro@users.noreply.github.com>
2022-09-28 12:35:29 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
 public class main{
     public static void main(String[] args) {
 
-        p = new Produto('teste', 'aoba', 5.5);
-        l = new Lote(10, new Date(2022,02,05), p);
+        Produto p = new Produto('teste', 'aoba', 5.5);
+        Lote l = new Lote(10, new Date(2022,02,05), p);
 
     }
 }

ae178be4aaccb174fa4d04a5b90ad2405f6b95e5
Bruno Grangeiro <brunobgrangeiro@gmail.com>
2022-10-03 08:14:09 -0300

    teste

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class main{
     public static void main(String[] args) {
 
         Produto p = new Produto('teste', 'aoba', 5.5);
-        Lote l = new Lote(10, new Date(2022,02,05), p);
+        Lote l = new Lote(10, new Date(2022,02,15), p);
 
     }
 }

66fc2c4b231bc457ad060cdb1f28793b1f8b7f69
Bruno Grangeiro <brunobgrangeiro@gmail.com>
2022-10-03 09:39:31 -0300

    alterações get/set, toString

--- Lote.java
+++ Lote.java
@@ -1,11 +1,44 @@
 public class Lote{
-    Int qtd;
-    date dataV;
-    produto produto;
+    int qtd;
+    String dataV;
+    Produto produto;
 
-    private Lote(int qtd, date dataV, produto produto){
+    Lote(int qtd, String dataV, Produto produto){
         this.qtd = qtd;
         this.produto = produto;
         this.dataV = dataV;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public String getDataV() {
+        return dataV;
+    }
+
+    public void setDataV(String dataV) {
+        this.dataV = dataV;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Lote{" +
+                "qtd=" + qtd +
+                ", dataV=" + dataV +
+                ", produto=" + produto +
+                '}';
     }
 }
--- main.java
+++ main.java
@@ -1,8 +1,9 @@
 public class main{
     public static void main(String[] args) {
 
-        Produto p = new Produto('teste', 'aoba', 5.5);
-        Lote l = new Lote(10, new Date(2022,02,15), p);
+        Produto p = new Produto("teste", "aoba", 5.5);
+        Lote l = new Lote(10, "22-05-2022", p);
 
+        System.out.println(l);
     }
 }
